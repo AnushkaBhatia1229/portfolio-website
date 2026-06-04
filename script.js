aa8fa28f68b547f4fd7c0b8a4198f384763d0465
Anushka Bhatia <2024464375.anushka@ug.sharda.ac.in>
2026-06-04 14:37:45 +0530

    Added Resume

--- script.js
+++ script.js
@@ -1,23 +1,18 @@
-function scrollToAbout() {
-    document.getElementById("about").scrollIntoView({
-        behavior: "smooth"
-    });
+const toggle = document.getElementById("theme-toggle");
+
+toggle.addEventListener("click", () => {
+
+document.body.classList.toggle("dark");
+
+if(document.body.classList.contains("dark")){
+localStorage.setItem("theme","dark");
+}
+else{
+localStorage.setItem("theme","light");
 }
 
-const themeBtn = document.getElementById("theme-btn");
-
-themeBtn.addEventListener("click", function () {
-    document.body.classList.toggle("dark-mode");
 });
 
-const form = document.getElementById("contactForm");
-
-form.addEventListener("submit", function (e) {
-
-    e.preventDefault();
-
-    document.getElementById("success-msg").innerText =
-        "Message Sent Successfully!";
-
-    form.reset();
-});
+if(localStorage.getItem("theme")==="dark"){
+document.body.classList.add("dark");
+}
